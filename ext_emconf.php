6e593e4a6ed259f2a13d9e3e61068142a8ea9be7
chrisb <chrisb@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-10-16 09:17:40 +0000

    feature: index content elements attached to news (ext:news). Thanks to TRITUM GmbH for sponsoring.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ke_search/trunk@89908 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 		    'php' => '5.3.0-0.0.0',
-		    'typo3' => '4.5.0-7.99.99',
+		    'typo3' => '4.5.0-7.4.99',
 		),
 		'conflicts' => '',
 		'suggests' => array(
